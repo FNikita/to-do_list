5d5c90968ccae5b24e310bc89d043d0a9f2e13d5
Nik <nik.fuchkin@gmail.com>
2019-10-09 21:50:55 +0300

    staly

--- webApp/to_do_list/models.py
+++ webApp/to_do_list/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class itemLI(models.Model):
     check = models.BooleanField()
     text = models.TextField()
+
+    def __str__(self):
+        return self.text
--- webApp/to_do_list/views.py
+++ webApp/to_do_list/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
+
+from .models import itemLI
 
 # Create your views here.
 
-
-
 def index(request):
-    return HttpResponse("It is my application!")
+    items = itemLI.objects.order_by('id')[:]
+    template = loader.get_template('index.html')
+    context = {
+        'items' : items,
+    }
+    return HttpResponse(template.render(context, request))
